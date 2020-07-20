25f0c4c570de31545f864242b73df22d0d6c1677
Arcoos2602 <cordonnier.thomas2@gmail.com>
2020-07-20 19:40:32 +0200

    version 2

--- app2.rb
+++ app2.rb
@@ -4,16 +4,42 @@ Bundler.require
 require_relative 'lib/player'
 require_relative 'lib/game'
 
-puts "A ma droite Josiane !"
+puts "---------------------------------------"
+puts "---BIENVENUE SUR RAID SHADOW LEGENDS---"
+puts "---------------------------------------\n\n"
+
+puts "Quel est ton nom Guerrier ?"
+print "> "
+name = gets.chomp
+human_player = HumanPlayer.new(name)
 player1 = Player.new("Josiane")
-puts "A ma gauche José !"
 player2 = Player.new("José")
-while player1.life_points > 0 && player2.life_points > 0
-  puts "Voici l'état des joueurs : \n \n"
-  puts player1.show_state
-  puts "#{player2.show_state} \n \n"
-  puts "Passons à la phase d'attaque : \n \n"
-  player1.attacks(player2)
-  if player2.life_points <= 0 then break end
-  player2.attacks(player1)
+ennemies = [player1, player2]
+puts "Les guerriers adverses qui te feront la peeeeeeau :"
+ennemies.each {|ennemy| puts ennemy.name}
+
+while human_player.life_points > 0 && (player1.life_points > 0 || player2.life_points > 0)
+  puts "#{human_player.show_state}\n\n"
+  puts "Quel action veux tu effectuer ?\n\n"
+  puts "a - chercher une meilleure arme"
+  puts "s - chercher à se soigner\n\n"
+  puts "attaquer un joueur en vue :"
+  puts "0 - #{player1.show_state}"
+  puts "1 - #{player2.show_state}"
+  print "> "
+  action = gets.chomp
+  if action == 'a' then human_player.search_weapon end
+  if action == 's' then human_player.search_health_pack end
+  if action == '0' then human_player.attacks(player1) end
+  if action == '1' then human_player.attacks(player2) end
+  puts "Les autres t'attaquent\n\n"
+  ennemies.each do |ennemy| 
+    if human_player.life_points > 0 && ennemy.life_points > 0
+      ennemy.attacks(human_player)
+    end
+  end
+  puts "\n\n"
 end
+
+if human_player.life_points > 0 then puts "GG à toi GRAND GUERRIER TOOOOOOOP 1" end
+if human_player.life_points < 0 then puts "LOOSEEEER" end
--- lib/player.rb
+++ lib/player.rb
@@ -8,7 +8,10 @@ class Player
   end
 
   def show_state
-    "#{@name} a #{@life_points} points de vie"
+    if @life_points <= 0 then "#{@name} est mort"
+    else
+      "#{@name} a #{@life_points} points de vie"
+    end
   end
 
   def gets_damage(points)
@@ -25,5 +28,48 @@ class Player
 
   def compute_damage
     return rand(1..6)
+  end
+end
+
+class HumanPlayer < Player
+  attr_accessor :weapon_level
+
+  def initialize(name_to_save)
+    @name = name_to_save
+    @life_points = 100
+    @weapon_level = 1
+  end
+
+  def show_state
+    "#{@name} a #{@life_points} points de vie et a une arme de niveau #{weapon_level}"
+  end
+
+  def compute_damage
+    rand(1..6) * @weapon_level
+  end
+
+  def search_weapon
+    level = rand(1..6)
+    puts "Tu as trouvé une arme de niveau #{level}"
+    if level < @weapon_level then puts "FUUUUUCK, arme caca :  tu la laisse" end
+    if level > @weapon_level
+      @weapon_level = level
+      puts "YEEEES, cet arme est METAL : tu la prends"
+    end
+  end
+
+  def search_health_pack
+    level = rand(1..6)
+    if level == 1 then puts "Tu n'as rien trouvé" end
+    if level >= 2 && level <= 5
+      @life_points += 50
+      if @life_points > 100 then @life_points = 100 end
+      puts "Bravo tu as trouvé un pack de 50 points de vie"
+    end
+    if level == 6
+      @life_points += 50
+      if @life_points > 100 then @life_points = 100 end
+      puts "Bravo tu as trouvé un pack de 80 points de vie MIAM"
+    end
   end
 end
